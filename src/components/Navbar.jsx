de5c5f664e1bb73d5755d4ba566b867789330935
fakerr-sketch <fm3209742@gmail.com>
2022-09-16 18:16:06 -0300

    create:components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>
+                        <Link to='/test'>Home</Link>
+                    </li>
+                    <li>
+                        <Link to='/animals'>Animals</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet />
+        </>
+    );
+}
+export default Navbar;
