af7ebaa578d5b65ddc29aca349142430f94dc9a0
fakerr-sketch <fm3209742@gmail.com>
2022-09-16 18:10:24 -0300

    create:pages

---
+++ src/pages/Animals.jsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+function Animals() {
+    return (
+        <>
+        
+        </>
+    )
+}
+
+export default Animals;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function HomePage() {
+    return (
+        <main>
+            <h1>Zoo</h1>
+            <p>The best zoo in the planet!</p>
+        </main>
+    );
+}
+
+export default HomePage;
