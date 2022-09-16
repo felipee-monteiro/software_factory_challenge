f1c8f02ee0ad41e127924291e8ade0dc54c5a2f2
fakerr-sketch <fm3209742@gmail.com>
2022-09-16 18:15:08 -0300

    create:router-provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
+import {RouterProvider} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import {routes} from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+  return <RouterProvider router={routes}/>;
 }
 
 export default App;
