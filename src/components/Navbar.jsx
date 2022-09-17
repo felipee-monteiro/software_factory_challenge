14b600a24f1290dad43fa816ed8d558535501072
fakerr-sketch <fm3209742@gmail.com>
2022-09-16 22:39:40 -0300

    update:component:navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,13 +4,13 @@ import { Link, Outlet } from 'react-router-dom';
 function Navbar() {
     return (
         <>
-            <nav>
+            <nav className='center'>
                 <ul>
                     <li>
-                        <Link to='/test'>Home</Link>
+                        <Link to='/'>Home</Link>
                     </li>
                     <li>
-                        <Link to='/animals'>Animals</Link>
+                        <Link to='/animals'>Animais</Link>
                     </li>
                     <li>
                         <Link to='/galery'>Galeria</Link>
