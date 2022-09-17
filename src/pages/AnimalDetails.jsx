c001e5622e6cca3619bc79d086c6f54d64971922
fakerr-sketch <fm3209742@gmail.com>
2022-09-17 20:47:54 -0300

    create:page:animal_details

---
+++ src/pages/AnimalDetails.jsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import {useParams} from 'react-router-dom';
+import {Context} from '../Providers/Context';
+
+function AnimalDetails() {
+    const {animals} = React.useContext(Context);
+    const {animal_name} = useParams();
+
+    const animal = animals.filter(animal => animal.name === animal_name)[0];
+
+    return (
+        <>
+            <main className='center'>
+                <section className='card'>
+                    <h1>{animal.name}</h1>
+                    <img src={animal.image_link} width='200' height='200' alt={animal.name} />
+                    <p>Nome em Latim: <i>{animal.latin_name}</i></p>
+                    <p>Habitat Natural: <i>{animal.habitat}</i></p>
+                    <p>Dieta: {animal.diet}</p>
+                    <p>Localização: {animal.geo_range}</p>
+                </section>
+            </main>
+        </>
+    );
+}
+
+export default AnimalDetails;
