775a386c77619bb7e108ee36f8da4a6c2c07db6a
fakerr-sketch <fm3209742@gmail.com>
2022-09-16 21:48:26 -0300

    create:page:animals

--- src/pages/Animals.jsx
+++ src/pages/Animals.jsx
@@ -1,9 +1,27 @@
 import * as React from 'react';
 
 function Animals() {
+    const [animal, setAnimal] = React.useState({});
+
+    const getAnimal = async () => {
+      const response = await fetch("https://zoo-animal-api.herokuapp.com/animals/rand");
+      return await response.json();
+    }
+
+    const updateAnimal = () => getAnimal().then(setAnimal);
+
+    React.useEffect(() => {
+      updateAnimal();
+    }, []);
+
     return (
         <>
-          <h1>Animals page</h1>
+          <h1>{animal.name}</h1>
+          <img src={animal.image_link} width='200' height='200' alt={animal.name} />
+          <p>Nome em Latim: <i>{animal.latin_name}</i></p>
+          <p>Habitat Natural: <i>{animal.habitat}</i></p>
+          <p>Dieta: {animal.diet}</p>
+          <button onClick={updateAnimal}>Gerar animal</button>
         </>
     )
 }
