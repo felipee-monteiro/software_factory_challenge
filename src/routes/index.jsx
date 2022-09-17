2cadfe03f97582baa234e0dfec68fb1a6229b9f8
fakerr-sketch <fm3209742@gmail.com>
2022-09-16 21:51:45 -0300

    update:routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
-import {
-    createBrowserRouter
-} from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import HomePage from '../pages/Home';
+import Animals from '../pages/Animals';
+import Galery from '../pages/Galery';
 
 export const routes = createBrowserRouter([
     {
@@ -15,7 +15,12 @@ export const routes = createBrowserRouter([
                 element: <HomePage />
             },
             {
-                path: '/animals'
+                path: '/animals',
+                element: <Animals />
+            },
+            {
+                path: '/galery',
+                element: <Galery />
             }
         ]
     }
