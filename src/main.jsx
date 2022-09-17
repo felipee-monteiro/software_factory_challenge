cbf6c957ad7a036751b7dad5790aab0b8dde30d1
fakerr-sketch <fm3209742@gmail.com>
2022-09-17 16:03:49 -0300

    add:context-api:providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {FavoritesProvider} from './Providers/FavoritesProvider';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '../public/styles/styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <FavoritesProvider>
+        <ToastContainer autoClose={2000} theme='dark' />
+        <App />
+    </FavoritesProvider>
+);
