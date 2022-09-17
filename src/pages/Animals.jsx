bcd888cedc39b6a8010465f7334c816e03e22e65
fakerr-sketch <fm3209742@gmail.com>
2022-09-16 22:40:51 -0300

    update:styles

--- src/pages/Animals.jsx
+++ src/pages/Animals.jsx
@@ -1,29 +1,34 @@
 import * as React from 'react';
 
 function Animals() {
-    const [animal, setAnimal] = React.useState({});
+  const [animal, setAnimal] = React.useState({});
 
-    const getAnimal = async () => {
-      const response = await fetch("https://zoo-animal-api.herokuapp.com/animals/rand");
-      return await response.json();
-    }
+  const getAnimal = async () => {
+    const response = await fetch("https://zoo-animal-api.herokuapp.com/animals/rand");
+    return await response.json();
+  }
 
-    const updateAnimal = () => getAnimal().then(setAnimal);
+  const updateAnimal = () => getAnimal().then(setAnimal);
 
-    React.useEffect(() => {
-      updateAnimal();
-    }, []);
+  React.useEffect(() => {
+    updateAnimal();
+  }, []);
 
-    return (
-        <>
+  return (
+    <>
+      <main className='center'>
+        <section className='card'>
           <h1>{animal.name}</h1>
           <img src={animal.image_link} width='200' height='200' alt={animal.name} />
           <p>Nome em Latim: <i>{animal.latin_name}</i></p>
           <p>Habitat Natural: <i>{animal.habitat}</i></p>
           <p>Dieta: {animal.diet}</p>
-          <button onClick={updateAnimal}>Gerar animal</button>
-        </>
-    )
+          <p>Localização: {animal.geo_range}</p>
+          <button className='btn' onClick={updateAnimal}>Gerar animal</button>
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default Animals;
